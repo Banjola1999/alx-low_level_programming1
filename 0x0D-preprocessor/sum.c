19deaf7125fe0b16d5eba2b62338accd26fa5d69
Banjola1999 <banjolaadesina@gmail.com>
2023-03-20 11:01:18 -0700

    4. SUM

---
+++ 0x0D-preprocessor/sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#ifndef OBJECT_LIKE_MARCO_H
+#define  OBJECT_LIKE_MARCO_H
+
+#define size 1024
+
+#endif 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main (void)
+{
+	int s;
+	s = 98 + size; 
+	printf ("%d\n",s);
+	return (0);
+}
